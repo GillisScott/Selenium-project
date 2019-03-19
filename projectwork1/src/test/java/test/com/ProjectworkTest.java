5d8431cc75e6c8d42ae2ff196510d7badc61cce2
GillisScott <47929365+GillisScott@users.noreply.github.com>
2019-03-19 12:33:01 -0500

    Add files via upload

--- projectwork1/src/test/java/test/com/ProjectworkTest.java
+++ projectwork1/src/test/java/test/com/ProjectworkTest.java
@@ -1,9 +1,9 @@
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package test.com;
 
 import java.util.concurrent.TimeUnit;
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -19,11 +20,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
- *
- * @author Hashim
- */
+*
+* @author Scott
+*/
 
 public class ProjectworkTest {
+    
     private WebDriver driver;
     
     public ProjectworkTest() {
@@ -38,13 +40,13 @@ public class ProjectworkTest {
     }
 
     @BeforeMethod
-    public void setUpMethod() throws Exception {
+    public void setUp() {
         
-        System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
-
-        driver = new ChromeDriver();
-        Thread.sleep(7000);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+     System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+     ChromeOptions chromeOptions= new ChromeOptions();
+     chromeOptions.setBinary("C:\\Users\\thest\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+     driver = new ChromeDriver(chromeOptions);
+     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @AfterMethod
@@ -55,6 +57,7 @@ public class ProjectworkTest {
     //@Test
     public void testItexpsPageTabAccess() throws Exception {
         driver.get("https://www.itexps.net/");
+        Thread.sleep(5000);
         // The following will traverse the ITEXPS Page Tabs and Login
         driver.findElement(By.id("DrpDwnMn00label")).click();
         Thread.sleep(2000);
@@ -92,14 +95,15 @@ public class ProjectworkTest {
 
 // Modifications using F12 / XPATH Code
 
-    //@Test
-    public void testLogin1() throws Exception {
+//@Test
+   public void testLogin1() throws Exception {
         driver.get("https://www.itexps.net/");
+        Thread.sleep(5000);
         //driver.findElement(By.id("iew9cwvzactionTitle")).click();
         driver.findElement(By.xpath("//*[@id=\"iew9cwvzactionTitle\"]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.id("signUpDialogswitchDialogLink")).click();
-        //driver.findElement(By.xpath("//*[@id=\"signUpDialogswitchDialogLink\"]")).click();
+        Thread.sleep(5000);
+        //driver.findElement(By.id("signUpDialogswitchDialogLink")).click();
+        driver.findElement(By.xpath("//*[@id=\"signUpDialogswitchDialogLink\"]")).click();
         //driver.findElement(By.id("memberLoginDialogemailInputinput")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"memberLoginDialogemailInputinput\"]")).click();      
@@ -114,39 +118,33 @@ public class ProjectworkTest {
         //driver.findElement(By.xpath("//*[@id=\"DrpDwnMn05label\"]")).click();
         driver.findElement(By.id("iew9cwvzactionTitle")).click();
     }
-  
-   @Test
-    public void testLoginAgain() throws Exception {
-        driver.get("https://www.google.com/");
-        driver.findElement(By.name("q")).click();
-        driver.findElement(By.name("q")).clear();
-        driver.findElement(By.name("q")).sendKeys("itexps");
-        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Web Result with Site Links'])[1]/following::h3[1]")).click();
-        driver.findElement(By.id("DrpDwnMn03label")).click();
-        driver.findElement(By.id("iew9cwvzactionTitle")).click();
-        // Sleep Delay for 3 seconds
-        Thread.sleep(3000);
+    
+ //@Test
+  public void testLoginFail() throws Exception {
+         driver.get("https://www.itexps.net/");
+        Thread.sleep(5000);
+        //driver.findElement(By.id("iew9cwvzactionTitle")).click();
+        driver.findElement(By.xpath("//*[@id=\"iew9cwvzactionTitle\"]")).click();
+        Thread.sleep(5000);
+        //driver.findElement(By.id("signUpDialogswitchDialogLink")).click();
         driver.findElement(By.xpath("//*[@id=\"signUpDialogswitchDialogLink\"]")).click();
-        driver.findElement(By.id("memberLoginDialogemailInputinput")).click();
-        driver.findElement(By.id("memberLoginDialogemailInputinput")).clear();
-        driver.findElement(By.id("memberLoginDialogemailInputinput")).sendKeys("thestorm10@sbcglobal.net");
+        //driver.findElement(By.id("memberLoginDialogemailInputinput")).click();
         Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"memberLoginDialogemailInputinput\"]")).click();      
+        driver.findElement(By.xpath("//*[@id=\"memberLoginDialogemailInputinput\"]")).clear();
+        driver.findElement(By.id("memberLoginDialogemailInputinput")).sendKeys("example123@hotmail.com");
         driver.findElement(By.id("memberLoginDialogpasswordInputinput")).click();
         driver.findElement(By.id("memberLoginDialogpasswordInputinput")).clear();
-        driver.findElement(By.id("memberLoginDialogpasswordInputinput")).sendKeys("Band1t-Boy");
-        Thread.sleep(2000);
+        driver.findElement(By.id("memberLoginDialogpasswordInputinput")).sendKeys("test123");
         driver.findElement(By.id("memberLoginDialogokButton")).click();
         Thread.sleep(2000);
-        // Job Seeker Tab from Menu Bar
-        //driver.findElement(By.id("DrpDwnMn03label")).click();
-        driver.findElement(By.xpath("//*[@id=\"DrpDwnMn03label\"]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"iew9cwvzactionTitle\"]")).click();
+        driver.findElement(By.id("DrpDwnMn05label")).click();
+        //driver.findElement(By.xpath("//*[@id=\"DrpDwnMn05label\"]")).click();
+        driver.findElement(By.id("iew9cwvzactionTitle")).click();
     }
-       
-    //@Test
-    public void testProjectSignup() throws Exception {
+           
+//@Test
+    public void testProjectSignupExists() throws Exception {
         driver.get("https://www.google.com/");
         driver.findElement(By.name("q")).click();
         driver.findElement(By.name("q")).clear();
@@ -173,6 +171,31 @@ public class ProjectworkTest {
         driver.findElement(By.id("memberLoginDialogpasswordInputinput")).clear();
         driver.findElement(By.id("memberLoginDialogpasswordInputinput")).sendKeys("Band1t-Boy");
         driver.findElement(By.id("memberLoginDialogokButton")).click();
+    }
+    
+
+@Test
+    public void testProjectSignupNew() throws Exception {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("itexps");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Web Result with Site Links'])[1]/following::h3[1]")).click();
+        Thread.sleep(5000);     // Using sleep() to allow Page to download and sync-up before continuing.
+        driver.findElement(By.id("iew9cwvzactionTitle")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.id("signUpDialogemailInputinput")).click();
+        driver.findElement(By.xpath("//*[@id=\"signUpDialogemailInputinput\"]")).clear();
+        Thread.sleep(2000);
+        driver.findElement(By.id("signUpDialogemailInputinput")).sendKeys("helloworld2@hotmail.com");
+        driver.findElement(By.id("signUpDialogpasswordInputinput")).click();
+        driver.findElement(By.id("signUpDialogpasswordInputinput")).clear();
+        driver.findElement(By.id("signUpDialogpasswordInputinput")).sendKeys("test123");
+        Thread.sleep(4000);
+        driver.findElement(By.id("signUpDialogokButton")).click();
+        Thread.sleep(4000);
+
     }
   
     //@Test
